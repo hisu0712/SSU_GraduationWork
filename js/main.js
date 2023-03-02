c5c997b0a7c19e13bab41a7c48deb11fae671bf7
Heesu <104744482+hisu0712@users.noreply.github.com>
2023-03-02 15:26:57 +0900

    Fix: 깃 베포 페이지에서 버그 수정

--- js/main.js
+++ js/main.js
@@ -567,13 +567,13 @@
     });
 
     window.addEventListener('resize', () => {
-      if (window.innerWidth > 900){
-        window.location.reload();
-        /*3번 scene 제외하고는 제대로 동작 
+        /*if (window.innerWidth > 900){
+          window.location.reload();
+          }*/
+        /*3번 scene 제외하고는 제대로 동작*/ 
         setLayout(); // 모바일에서는 리사이즈X
         sceneInfo[3].values.rectStartY = 0; // 리사이즈 대응
-        */
-      }
+      
     }); //창을 세로로 줄일 때, 다시 높이 세팅
 
     window.addEventListener('orientationchange', () => {
